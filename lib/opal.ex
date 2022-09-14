e7f0ba464e3ee405a4b33d5d59d7b08c06905cf2
Jon-Eric Cook <joneric.cook@gmail.com>
2022-09-14 16:08:38 -0700

    first commit

---
+++ lib/opal.ex
@@ -0,0 +1,18 @@
+defmodule Opal do
+  @moduledoc """
+  Documentation for `Opal`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Opal.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
